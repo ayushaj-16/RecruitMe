7d27168dd1d63455ba3f9126d0238eeee37d2c91
ayushaj-16 <jaiswalayush131313@gmail.com>
2022-06-27 00:21:50 +0530

    Basic Setup

---
+++ client/src/components/Logo.js
@@ -0,0 +1,7 @@
+import logo from '../assets/images/logo.png'
+
+const Logo = () => {
+  return <img src={logo} style={{marginTop: "10px"}} alt='RecruitMe' className='logo' />
+}
+
+export default Logo
